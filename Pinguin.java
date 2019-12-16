8c30f979d5a569fe1447a010299de5c2a52ac9a5
alinlpkv <alinlpkv@gmail.com>
2019-12-16 22:12:31 +0300

    first commit

---
+++ Pinguin.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+
+
+public class Pinguin extends Bird {
+    public Pinguin(){
+        System.out.println("Я пингвин!");
+
+        color= Color.ORANGE;
+        if(x<xmin){
+            xmin=x;
+        }
+        if(x>xmax){
+            xmax=x;
+        }
+        if(y>ymax){
+            ymax=y;
+        }
+        if(y<ymin){
+            ymin=y;
+        }
+        System.out.println("xmin= "+xmin);
+        System.out.println("xmax= "+xmax);
+        System.out.println("ymin= "+ymin);
+        System.out.println("ymax= "+ymax);
+
+    }
+    @Override
+    public void fly(){
+        System.out.println("Я не хочу летать!");
+    }
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x,y,size,size);
+        g.setColor(Color.white);
+        g.fillOval(x+size/2-(size-5)/2,y+size/2-(size-5)/2,size-5,size-5);
+        g.setColor(Color.red);
+
+
+        g.drawRect(xmin,ymin,xmax-xmin+size,ymax-ymin+size);
+    }
+
+}
